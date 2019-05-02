dc061c807c371f5fc7151c164017434b7f82d90e
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-05-02 14:09:22 +0800

    init

--- QKL_ETHUSDT.py
+++ QKL_ETHUSDT.py
@@ -6,15 +6,15 @@ import ccxt
 import talib as ta
 from email_util import *
 
-huobi = ccxt.huobipro()
+gateio = ccxt.gateio()
 
 limit = 500
 current_time = int(time.time()//60*60*1000)
 print(current_time)
 
-since_time = current_time - limit * 60 * 60 * 1000
+since_time = current_time - limit * 4 * 60 * 60 * 1000
 
-data = huobi.fetch_ohlcv(symbol='TRX/USDT',timeframe='1h', limit=500,since=since_time)
+data = gateio.fetch_ohlcv(symbol='ETH/USDT',timeframe='4h', limit=500,since=since_time)
 df = pd.DataFrame(data)
 df = df.rename(columns={0: 'open_time', 1: 'open', 2: 'high', 3: 'low', 4: 'close', 5: 'volume'})
 df['open_time'] = pd.to_datetime(df['open_time'], unit='ms') + pd.Timedelta(hours=8)
@@ -30,4 +30,10 @@ doubleLowArray = num.asarray(lowArray, dtype='double')
 
 upperband, middleband, lowerband = ta.BBANDS(doubleCloseArray, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
 
-sendMail("111111111111111","11111111111")
+if (lowArray[-1] <= lowerband[-1]):
+    sendMail("【GATE.IO】【ETH/USDT】触发4小时布林线下沿", "【GATE.IO】【ETH/USDT】触发4小时布林线下沿")
+if (highArray[-1] >= upperband[-1]):
+    sendMail("【GATE.IO】【ETH/USDT】触发4小时布林线上沿", "【GATE.IO】【ETH/USDT】触发4小时布林线上沿")
+
+
+
