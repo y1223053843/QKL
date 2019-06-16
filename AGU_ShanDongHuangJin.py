32d696fbc946883762cbe1b57b2d7aee3cbfc072
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-06-16 22:00:37 +0800

    init

--- AGU_ShanDongHuangJin.py
+++ AGU_ShanDongHuangJin.py
@@ -9,9 +9,6 @@ from email_util import *
 a = 0
 
 def strategy(code, name, zhouqi):
-     global a
-     if (a == 1):
-          return
 
      if (zhouqi == '30'):
           zhouqi_ch = "30分钟"
@@ -35,6 +32,9 @@ def strategy(code, name, zhouqi):
 
      upperband, middleband, lowerband = ta.BBANDS(doubleCloseArray, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
 
+     fastk, fastd = ta.STOCHRSI(num.asarray(doubleCloseArray, dtype='double'), timeperiod=14, fastk_period=14, fastd_period=3, fastd_matype=3)
+     #print(fastd)
+
      print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
      print(zhouqi_ch + "LOWER===============" + str(lowArray[-1]))
      print(zhouqi_ch + "HIGHER==============" + str(highArray[-1]))
@@ -43,13 +43,28 @@ def strategy(code, name, zhouqi):
      print(zhouqi_ch + "BULL middleband=====" +  str(middleband[-1]))
      print(zhouqi_ch + "BULL lowerband======" +  str(lowerband[-1]))
 
-     if ((lowArray[-1] - lowerband[-1])/lowArray[-1] <= 0.01 ):
-          a = 1
-          sendMail(name + "触发" + zhouqi_ch + "布林线下沿,当前价格：" + str(closeArray[-1]), name + "触发" + zhouqi_ch + "布林线下沿,当前价格：" + str(closeArray[-1]))
-     if (highArray[-1] >= upperband[-1]):
-          a = 1
-          sendMail(name + "触发" + zhouqi_ch + "布林线上沿,当前价格：" + str(closeArray[-1]), name + "触发" + zhouqi_ch + "布林线上沿,当前价格：" + str(closeArray[-1]))
+     if (zhouqi == '60'):
+          if (fastd[-1] < 5):
+               sendMail(name + "触" + zhouqi_ch + "STOTCHRSI底部：" + str(closeArray[-1]) + " RSI_1h:" + "%.2f" % fastd[
+                    -2] + "_" + "%.2f" % fastd[-1],
+                        name + "触" + zhouqi_ch + "STOTCHRSI底部：" + str(closeArray[-1]) + " RSI_1h:" + "%.2f" % fastd[
+                             -2] + "_" + "%.2f" % fastd[-1])
+          if (fastd[-1] > 90):
+               sendMail(name + "触" + zhouqi_ch + "STOTCHRSI顶部：" + str(closeArray[-1]) + " RSI_1h:" + "%.2f" % fastd[
+                    -2] + "_" + "%.2f" % fastd[-1],
+                        name + "触" + zhouqi_ch + "STOTCHRSI顶部：" + str(closeArray[-1]) + " RSI_1h:" + "%.2f" % fastd[
+                             -2] + "_" + "%.2f" % fastd[-1])
 
-strategy("600547", "山东黄金", "30")
+     # global a
+     # if (a == 1):
+     #      return
+     #
+     # if ((lowArray[-1] - lowerband[-1])/lowArray[-1] <= 0.01 ):
+     #      a = 1
+     #      sendMail(name + "触发" + zhouqi_ch + "布林线下沿,当前价格：" + str(closeArray[-1]), name + "触发" + zhouqi_ch + "布林线下沿,当前价格：" + str(closeArray[-1]))
+     # if (highArray[-1] >= upperband[-1]):
+     #      a = 1
+     #      sendMail(name + "触发" + zhouqi_ch + "布林线上沿,当前价格：" + str(closeArray[-1]), name + "触发" + zhouqi_ch + "布林线上沿,当前价格：" + str(closeArray[-1]))
+
 strategy("600547", "山东黄金", "60")
 strategy("600547", "山东黄金", "D")
