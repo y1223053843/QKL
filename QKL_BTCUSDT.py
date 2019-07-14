972f965fae3a46be42b022cec2492e7c18ae9775
LAPTOP-Q4S5BFD2\yangj <317491841@qq.com>
2019-07-14 09:46:48 +0800

    init

--- QKL_BTCUSDT.py
+++ QKL_BTCUSDT.py
@@ -20,12 +20,15 @@ def strategy(name,zhouqi):
         data = gateio.fetch_ohlcv(symbol=name,timeframe='15m', limit=500,since=since_time)
         zhouqi_ch = "15分钟"
     if (zhouqi == '1h'):
-        since_time = current_time - limit * 1* 60 * 60 * 1000
-        data = gateio.fetch_ohlcv(symbol=name, timeframe='1h', limit=500, since=since_time)
-
-        since_time_30 = current_time - limit * 1* 30 * 60 * 1000
         time.sleep(5)
-        data_30 = huobi.fetch_ohlcv(symbol=name, timeframe='30m', limit=500, since=since_time)
+        since_time = current_time - limit * 1* 60 * 60 * 1000
+        data = huobi.fetch_ohlcv(symbol=name, timeframe='1h', limit=500, since=since_time)
+        time.sleep(2)
+        since_time_15 = current_time - limit * 1 * 15 * 60 * 1000
+        data_15 = huobi.fetch_ohlcv(symbol=name, timeframe='15m', limit=500, since=since_time_15)
+        time.sleep(2)
+        since_time_30 = current_time - limit * 1* 30 * 60 * 1000
+        data_30 = huobi.fetch_ohlcv(symbol=name, timeframe='30m', limit=500, since=since_time_30)
 
         zhouqi_ch = "1h"
     if (zhouqi == '2h'):
@@ -52,11 +55,17 @@ def strategy(name,zhouqi):
     doubleLowArray = num.asarray(lowArray, dtype='double')
     doubleOpenArray = num.asarray(openArray, dtype='double')
 
+    # 30分钟
+    df_15 = pd.DataFrame(data_15)
+    df_15 = df_15.rename(columns={0: 'open_time', 1: 'open', 2: 'high', 3: 'low', 4: 'close', 5: 'volume'})
+    df_15['open_time'] = pd.to_datetime(df_15['open_time'], unit='ms') + pd.Timedelta(hours=8)
+    closeArray_15 = num.array(df_15['close'])
+    doubleCloseArray_15 = num.asarray(closeArray_15, dtype='double')
+
+    # 30分钟
     df_30 = pd.DataFrame(data_30)
     df_30 = df_30.rename(columns={0: 'open_time', 1: 'open', 2: 'high', 3: 'low', 4: 'close', 5: 'volume'})
     df_30['open_time'] = pd.to_datetime(df_30['open_time'], unit='ms') + pd.Timedelta(hours=8)
-
-    # 02、 数据格式处理、并计算布林线值
     closeArray_30 = num.array(df_30['close'])
     doubleCloseArray_30 = num.asarray(closeArray_30, dtype='double')
 
@@ -67,16 +76,16 @@ def strategy(name,zhouqi):
     middleband = middleband / 1000
     lowerband = lowerband / 1000
 
-    # macd 为快线 macdsignal为慢线，macdhist为柱体
-    # macd, macdsignal, macdhist = ta.MACD(num.asarray(doubleCloseArray_30*1000, dtype='double'), fastperiod=12, slowperiod=26,
-    #                                      signalperiod=9)
-    # macd = macd / 1000
-    # macdsignal = macdsignal / 1000
-    # macdhist = macdhist / 1000
-    # # print(doubleCloseArray)
-    # # print(doubleCloseArray_30)
+    #macd 为快线 macdsignal为慢线，macdhist为柱体
+    #print(doubleCloseArray_15)
+    macd, macdsignal, macdhist = ta.MACD(num.asarray(doubleCloseArray_15*1000, dtype='double'), fastperiod=12, slowperiod=26,
+                                         signalperiod=9)
+    macd = macd / 1000
+    macdsignal = macdsignal / 1000
+    macdhist = macdhist / 1000
+
     # str1 = ""
-    # if (macdhist[-1] > macdhist[-2] and macdhist[-3] > macdhist[-2]):
+    # if (macdsignal[-1] > macdhist[-2] and macdhist[-3] > macdhist[-2]):
     #     str1 = "底部转折"
     # if (macdhist[-1] < macdhist[-2] and macdhist[-3] < macdhist[-2]):
     #     str1 = "顶部转折"
@@ -86,6 +95,7 @@ def strategy(name,zhouqi):
         global fastd_1h
         fastd_1h = fastd
 
+    #print(doubleCloseArray_30)
     fastk_30, fastd_30 = ta.STOCHRSI(num.asarray(doubleCloseArray_30, dtype='double'), timeperiod=14, fastk_period=14,
                                fastd_period=3, fastd_matype=3)
 
@@ -103,14 +113,26 @@ def strategy(name,zhouqi):
     # print(zhouqi_ch + "MACD 柱体===========" + str(macdhist[-1]))
     print(zhouqi_ch + "RSI_1h =============" + "%.2f" % fastd[-5] + "_" + "%.2f" % fastd[-4] + "_" + "%.2f" % fastd[-3] + "_" + "%.2f" % fastd[-2] + "_" + "%.2f" % fastd[-1])
 
+    strMA = " MA15:" + "%.1f" % macdhist[-2] + "/" + "%.1f" % macdhist[-1]
+    # print(macdsignal)
+    # print(macdhist)
+    if (macdsignal[-1] > 0 and macdhist[-2] < macdhist[-1]):
+        strMA = " MA15主买进:" + "%.1f" % macdhist[-2] + "/" + "%.1f" % macdhist[-1]
+        if (macdsignal[-2] < 0 and macdsignal[-1] > 0):
+            strMA = " MA15负正转折（多）:" + "%.6f" % macdhist[-2] + "/" + "%.1f" % macdhist[-1]
+    if (macdsignal[-1] < 0 and macdhist[-2] > macdhist[-1]):
+        strMA = " MA15主卖出:" + "%.1f" % macdhist[-2] + "/" + "%.1f" % macdhist[-1]
+        if (macdsignal[-2] > 0 and macdsignal[-1] < 0):
+            strMA = " MA15正负转折（空）:" + "%.6f" % macdhist[-2] + "/" + "%.1f" % macdhist[-1]
+
     name_jian = name[0:3]
     if (zhouqi == '1h'):
         if (fastd[-1] < 50):
-            sendMail(name_jian + "" + zhouqi_ch + ":" + str(closeArray[-1]) + " " + "%.1f" % fastd[-3] + "_" + "%.1f" % fastd[-2] + "_" + "%.1f" % fastd[-1] + " 30分:" + "%.1f" % fastd_30[-3] + "_" + "%.1f" % fastd_30[-2] + "_" + "%.1f" % fastd_30[-1] ,
-                     name_jian + "" + zhouqi_ch + ":" + str(closeArray[-1]) + " " + "%.1f" % fastd[-3] + "_" + "%.1f" % fastd[-2] + "_" + "%.1f" % fastd[-1] + " 30分:" + "%.1f" % fastd_30[-3] + "_" + "%.1f" % fastd_30[-2] + "_" + "%.1f" % fastd_30[-1])
+            sendMail(name_jian + "%.1f" % closeArray[-1] + " " + zhouqi_ch + ":" + "%.1f" % fastd[-3] + "/" + "%.1f" % fastd[-2] + "/" + "%.1f" % fastd[-1] + " 30分:" + "%.1f" % fastd_30[-3] + "/" + "%.1f" % fastd_30[-2] + "/" + "%.1f" % fastd_30[-1] + strMA,
+                     name_jian + "%.1f" % closeArray[-1] + " " + zhouqi_ch + ":" + "%.1f" % fastd[-3] + "/" + "%.1f" % fastd[-2] + "/" + "%.1f" % fastd[-1] + " 30分:" + "%.1f" % fastd_30[-3] + "/" + "%.1f" % fastd_30[-2] + "/" + "%.1f" % fastd_30[-1])+ strMA
         if (fastd[-1] > 50):
-            sendMail(name_jian + "" + zhouqi_ch + ":" + str(closeArray[-1]) + " " + "%.1f" % fastd[-3] + "_" + "%.1f" % fastd[-2] + "_" + "%.1f" % fastd[-1] + " 30分:" + "%.1f" % fastd_30[-3] + "_" + "%.1f" % fastd_30[-2] + "_" + "%.1f" % fastd_30[-1] ,
-                     name_jian + "" + zhouqi_ch + ":" + str(closeArray[-1]) + " " + "%.1f" % fastd[-3] + "_" + "%.1f" % fastd[-2] + "_" + "%.1f" % fastd[-1] + " 30分:" + "%.1f" % fastd_30[-3] + "_" + "%.1f" % fastd_30[-2] + "_" + "%.1f" % fastd_30[-1] )
+            sendMail(name_jian + "%.1f" % closeArray[-1] + " " + zhouqi_ch + ":" + "%.1f" % fastd[-3] + "/" + "%.1f" % fastd[-2] + "/" + "%.1f" % fastd[-1] + " 30分:" + "%.1f" % fastd_30[-3] + "/" + "%.1f" % fastd_30[-2] + "/" + "%.1f" % fastd_30[-1] + strMA,
+                     name_jian + "%.1f" % closeArray[-1] + " " + zhouqi_ch + ":" + "%.1f" % fastd[-3] + "/" + "%.1f" % fastd[-2] + "/" + "%.1f" % fastd[-1] + " 30分:" + "%.1f" % fastd_30[-3] + "/" + "%.1f" % fastd_30[-2] + "/" + "%.1f" % fastd_30[-1] + strMA)
 
     # global a
     # if (a == 1):
--- QKL_TRXUSDT.py
+++ QKL_TRXUSDT.py
@@ -21,10 +21,13 @@ def strategy(name,zhouqi):
         zhouqi_ch = "15分钟"
     if (zhouqi == '1h'):
         since_time = current_time - limit * 1* 60 * 60 * 1000
-        data = gateio.fetch_ohlcv(symbol=name, timeframe='1h', limit=500, since=since_time)
-
+        data = huobi.fetch_ohlcv(symbol=name, timeframe='1h', limit=500, since=since_time)
+        time.sleep(2)
+        since_time_15 = current_time - limit * 1 * 15 * 60 * 1000
+        data_15 = huobi.fetch_ohlcv(symbol=name, timeframe='15m', limit=500, since=since_time_15)
+        time.sleep(2)
         since_time_30 = current_time - limit * 1* 30 * 60 * 1000
-        data_30 = huobi.fetch_ohlcv(symbol=name, timeframe='30m', limit=500, since=since_time)
+        data_30 = huobi.fetch_ohlcv(symbol=name, timeframe='30m', limit=500, since=since_time_30)
 
         zhouqi_ch = "1h"
     if (zhouqi == '2h'):
@@ -51,11 +54,17 @@ def strategy(name,zhouqi):
     doubleLowArray = num.asarray(lowArray, dtype='double')
     doubleOpenArray = num.asarray(openArray, dtype='double')
 
+    # 30分钟
+    df_15 = pd.DataFrame(data_15)
+    df_15 = df_15.rename(columns={0: 'open_time', 1: 'open', 2: 'high', 3: 'low', 4: 'close', 5: 'volume'})
+    df_15['open_time'] = pd.to_datetime(df_15['open_time'], unit='ms') + pd.Timedelta(hours=8)
+    closeArray_15 = num.array(df_15['close'])
+    doubleCloseArray_15 = num.asarray(closeArray_15, dtype='double')
+
+    # 30分钟
     df_30 = pd.DataFrame(data_30)
     df_30 = df_30.rename(columns={0: 'open_time', 1: 'open', 2: 'high', 3: 'low', 4: 'close', 5: 'volume'})
     df_30['open_time'] = pd.to_datetime(df_30['open_time'], unit='ms') + pd.Timedelta(hours=8)
-
-    # 02、 数据格式处理、并计算布林线值
     closeArray_30 = num.array(df_30['close'])
     doubleCloseArray_30 = num.asarray(closeArray_30, dtype='double')
 
@@ -66,16 +75,16 @@ def strategy(name,zhouqi):
     middleband = middleband / 1000
     lowerband = lowerband / 1000
 
-    # macd 为快线 macdsignal为慢线，macdhist为柱体
-    # macd, macdsignal, macdhist = ta.MACD(num.asarray(doubleCloseArray_30*1000, dtype='double'), fastperiod=12, slowperiod=26,
-    #                                      signalperiod=9)
-    # macd = macd / 1000
-    # macdsignal = macdsignal / 1000
-    # macdhist = macdhist / 1000
-    # # print(doubleCloseArray)
-    # # print(doubleCloseArray_30)
+    #macd 为快线 macdsignal为慢线，macdhist为柱体
+    #print(doubleCloseArray_15)
+    macd, macdsignal, macdhist = ta.MACD(num.asarray(doubleCloseArray_15*1000, dtype='double'), fastperiod=12, slowperiod=26,
+                                         signalperiod=9)
+    macd = macd / 1000
+    macdsignal = macdsignal / 1000
+    macdhist = macdhist / 1000
+
     # str1 = ""
-    # if (macdhist[-1] > macdhist[-2] and macdhist[-3] > macdhist[-2]):
+    # if (macdsignal[-1] > macdhist[-2] and macdhist[-3] > macdhist[-2]):
     #     str1 = "底部转折"
     # if (macdhist[-1] < macdhist[-2] and macdhist[-3] < macdhist[-2]):
     #     str1 = "顶部转折"
@@ -85,6 +94,7 @@ def strategy(name,zhouqi):
         global fastd_1h
         fastd_1h = fastd
 
+    #print(doubleCloseArray_30)
     fastk_30, fastd_30 = ta.STOCHRSI(num.asarray(doubleCloseArray_30, dtype='double'), timeperiod=14, fastk_period=14,
                                fastd_period=3, fastd_matype=3)
 
@@ -102,14 +112,26 @@ def strategy(name,zhouqi):
     # print(zhouqi_ch + "MACD 柱体===========" + str(macdhist[-1]))
     print(zhouqi_ch + "RSI_1h =============" + "%.2f" % fastd[-5] + "_" + "%.2f" % fastd[-4] + "_" + "%.2f" % fastd[-3] + "_" + "%.2f" % fastd[-2] + "_" + "%.2f" % fastd[-1])
 
+    strMA = " MA15:" + "%.6f" % macdhist[-2] + "/" + "%.6f" % macdhist[-1]
+    # print(macdsignal)
+    # print(macdhist)
+    if (macdsignal[-1] > 0 and macdhist[-2] < macdhist[-1]):
+        strMA = " MA15主买进:" + "%.6f" % macdhist[-2] + "/" + "%.6f" % macdhist[-1]
+        if (macdsignal[-2] < 0 and macdsignal[-1] > 0):
+            strMA = " MA15负正转折（多）:" + "%.6f" % macdhist[-2] + "/" + "%.6f" % macdhist[-1]
+    if (macdsignal[-1] < 0 and macdhist[-2] > macdhist[-1]):
+        strMA = " MA15主卖出:" + "%.6f" % macdhist[-2] + "/" + "%.6f" % macdhist[-1]
+        if (macdsignal[-2] > 0 and macdsignal[-1] < 0):
+            strMA = " MA15正负转折（空）:" + "%.6f" % macdhist[-2] + "/" + "%.6f" % macdhist[-1]
+
     name_jian = name[0:3]
     if (zhouqi == '1h'):
         if (fastd[-1] < 50):
-            sendMail(name_jian + "" + zhouqi_ch + ":" + str(closeArray[-1]) + " " + "%.1f" % fastd[-3] + "_" + "%.1f" % fastd[-2] + "_" + "%.1f" % fastd[-1] + " 30分:" + "%.1f" % fastd_30[-3] + "_" + "%.1f" % fastd_30[-2] + "_" + "%.1f" % fastd_30[-1] ,
-                     name_jian + "" + zhouqi_ch + ":" + str(closeArray[-1]) + " " + "%.1f" % fastd[-3] + "_" + "%.1f" % fastd[-2] + "_" + "%.1f" % fastd[-1] + " 30分:" + "%.1f" % fastd_30[-3] + "_" + "%.1f" % fastd_30[-2] + "_" + "%.1f" % fastd_30[-1])
+            sendMail(name_jian + "%.5f" % closeArray[-1] + " " + zhouqi_ch + ":" + "%.1f" % fastd[-3] + "/" + "%.1f" % fastd[-2] + "/" + "%.1f" % fastd[-1] + " 30分:" + "%.1f" % fastd_30[-3] + "/" + "%.1f" % fastd_30[-2] + "/" + "%.1f" % fastd_30[-1] + strMA,
+                     name_jian + "%.5f" % closeArray[-1] + " " + zhouqi_ch + ":" + "%.1f" % fastd[-3] + "/" + "%.1f" % fastd[-2] + "/" + "%.1f" % fastd[-1] + " 30分:" + "%.1f" % fastd_30[-3] + "/" + "%.1f" % fastd_30[-2] + "/" + "%.1f" % fastd_30[-1])+ strMA
         if (fastd[-1] > 50):
-            sendMail(name_jian + "" + zhouqi_ch + ":" + str(closeArray[-1]) + " " + "%.1f" % fastd[-3] + "_" + "%.1f" % fastd[-2] + "_" + "%.1f" % fastd[-1] + " 30分:" + "%.1f" % fastd_30[-3] + "_" + "%.1f" % fastd_30[-2] + "_" + "%.1f" % fastd_30[-1] ,
-                     name_jian + "" + zhouqi_ch + ":" + str(closeArray[-1]) + " " + "%.1f" % fastd[-3] + "_" + "%.1f" % fastd[-2] + "_" + "%.1f" % fastd[-1] + " 30分:" + "%.1f" % fastd_30[-3] + "_" + "%.1f" % fastd_30[-2] + "_" + "%.1f" % fastd_30[-1] )
+            sendMail(name_jian + "%.5f" % closeArray[-1] + " " + zhouqi_ch + ":" + "%.1f" % fastd[-3] + "/" + "%.1f" % fastd[-2] + "/" + "%.1f" % fastd[-1] + " 30分:" + "%.1f" % fastd_30[-3] + "/" + "%.1f" % fastd_30[-2] + "/" + "%.1f" % fastd_30[-1] + strMA,
+                     name_jian + "%.5f" % closeArray[-1] + " " + zhouqi_ch + ":" + "%.1f" % fastd[-3] + "/" + "%.1f" % fastd[-2] + "/" + "%.1f" % fastd[-1] + " 30分:" + "%.1f" % fastd_30[-3] + "/" + "%.1f" % fastd_30[-2] + "/" + "%.1f" % fastd_30[-1] + strMA)
 
     # global a
     # if (a == 1):
